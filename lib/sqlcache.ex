5a2d945f62626c00d92dfeeef569a38aeafa0c55
Roman Heinrich <roman.heinrich@gmail.com>
2023-02-27 22:45:37 +0100

    init

---
+++ lib/sqlcache.ex
@@ -0,0 +1,18 @@
+defmodule Sqlcache do
+  @moduledoc """
+  Documentation for `Sqlcache`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sqlcache.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sqlcache_test.exs
@@ -0,0 +1,8 @@
+defmodule SqlcacheTest do
+  use ExUnit.Case
+  doctest Sqlcache
+
+  test "greets the world" do
+    assert Sqlcache.hello() == :world
+  end
+end
